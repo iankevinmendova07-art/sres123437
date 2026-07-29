1fd9ae8512199102da143af8bba33a52f08f450c
iankevinmendova07-art <iankevinmendova07@gmail.com>
2026-07-29 22:43:54 +0800

    Describe your update here

--- js/main.js
+++ js/main.js
@@ -73,22 +73,22 @@ function pagination() {
 // ==========================================
 const heroSlides = [
     {
-        image: "../img/hero1.jpeg",
+        image: "img/hero1.jpeg",
         alt: "School Activity 1",
         caption: "School Governance Council Election test"
     },
     {
-        image: "../img/hero2.jpeg",
+        image: "img/hero2.jpeg",
         alt: "School Activity 2",
         caption: "OPCRF 2025 VALIDATION test"
     },
     {
-        image: "../img/hero3.jpg",
+        image: "img/hero3.jpg",
         alt: "School Activity 3",
         caption: "Serving Every one in Devotion to Christ (SEED) Partnerships test"
     },
     {
-        image: "../img/hero4.jpg",
+        image: "img/hero4.jpg",
         alt: "School Activity 4",
         caption: "School Personnel Meeting test"
     }
